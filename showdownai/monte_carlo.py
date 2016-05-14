330acf81863b3f0b39ff0b9e1b4790d781c13a0d
Rohan Batra <rohbat@gmail.com>
2016-05-14 02:51:07 -0700

    removed montecarlo class, everything in Node

--- showdownai/monte_carlo.py
+++ showdownai/monte_carlo.py
@@ -1,3 +1,12 @@
 class MonteCarlo():
     def __init__(self, state):
+        self.root = Node(state)
+
+
+
+class Node():
+    def __init__(self, state, action=NULL):
         self.state = state
+        self.action = action
+
+
