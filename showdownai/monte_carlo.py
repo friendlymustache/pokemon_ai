2bf179afe9adaa292e4817bbbc832ebeeae8f94f
Rohan Batra <rohbat@gmail.com>
2016-05-14 16:56:39 -0700

    Added more class variables

--- showdownai/monte_carlo.py
+++ showdownai/monte_carlo.py
@@ -1,12 +1,14 @@
-class MonteCarlo():
+class MonteCarloTree():
     def __init__(self, state):
-        self.root = Node(state)
-
-
+        self.root = 
 
 class Node():
-    def __init__(self, state, action=NULL):
+    def __init__(self, state, who, action=NULL):
         self.state = state
+        self.who = who
+        self.possible_actions = state.get_legal_actions(who)
         self.action = action
+        self.ucb_score = 1 / self.possible_actions
 
 
+self.root = Node(state)
