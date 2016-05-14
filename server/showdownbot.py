94de812bf5d5cf45bd4f387b74754046707783b4
Nand Kishore <nandck@gmail.com>
2016-05-14 16:29:59 -0700

    Update poke features and fix error in move_predict.

--- server/showdownbot.py
+++ server/showdownbot.py
@@ -1,6 +1,6 @@
 import sys
-sys.path.append('~/Desktop/Caltech/Year 3 Term 3/CS159/pokemon_ai')
-print sys.path
+import os
+sys.path.append(os.path.abspath('./'))
 from server import main
 
 if __name__ == "__main__":
