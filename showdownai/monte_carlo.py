861a48dfcd99054b47e543c216bc5763ec26cf48
Rohan Batra <rohbat@gmail.com>
2016-05-14 17:09:11 -0700

    Added attributes

--- showdownai/monte_carlo.py
+++ showdownai/monte_carlo.py
@@ -3,12 +3,13 @@ class MonteCarloTree():
         self.root = 
 
 class Node():
-    def __init__(self, state, who, action=NULL):
+    def __init__(self, state, parent):
         self.state = state
-        self.who = who
         self.possible_actions = state.get_legal_actions(who)
-        self.action = action
-        self.ucb_score = 1 / self.possible_actions
+        self.parent = parent
+        self.children = []
+        self.my_ucb_score = 1 / len(self.possible_actions)
+        self.their_ucb_score = 1 / len(self.possible_actions)
 
 
 self.root = Node(state)
