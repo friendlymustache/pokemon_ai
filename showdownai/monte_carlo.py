e4a97a326bf1f3a83ce5adfd764fb99efaca2ba7
Rohan Batra <rohbat@gmail.com>
2016-05-17 18:33:50 -0700

    Implemented backpropogation

--- showdownai/monte_carlo.py
+++ showdownai/monte_carlo.py
@@ -17,8 +17,8 @@ class MonteCarloTree():
     def select_add_actionpair(self):
         current = self.root
 
-        my_action = max(current.my_actions, key=itemgetter(1))[0]
-        opp_action = max(current.opp_actions, key=itemgetter(1))[0]
+        my_action = max(current.my_actions, key=lambda i: current.my_actions[i])
+        opp_action = max(current.opp_actions, key=lambda i: current.opp_actions[i])
         key = (my_action, opp_action)
 
         while (key in current.children_actionpairs):
@@ -44,23 +44,16 @@ class MonteCarloTree():
     def back_propogate(self, node, outcome):
         # Node passed in is a GS node
         while(node.parent != None):
-            # Parent is action node; increment vars and get action
+            # Parent is action node; increment, get action pair to calculate uct
             node = node.parent
-            node.increment()
-            score = update_uct_scores(node)
+            node.increment(outcome)
+            ap = node.action_pair
 
+            # Update corresponding UCT score in previous GS node
             node = node.parent
-            node.increment()
+            node.increment(outcome, ap)
+            node.update_uct_scores(ap)
 
-
-    def update_uct_scores(self, node):
-        C = 0.5
-        ap = node.action_pair
-        mean = self.wins / self.times_visited
-        my_n = self.my_action_n[action_pair[0]]
-        opp_n = self.opp_action_n[action_pair[1]]
-        my_explore = 2 * C * np.sqrt(2 * np.log(self.times_visited) / my_n)
-        opp_explore = 2 * C * np.sqrt(2 * np.log(self.times_visited) / opp_n)
 
 
 class Node():
@@ -73,6 +66,7 @@ class Node():
         self.times_visited += 1
         self.wins += outcome
 
+
 class GameStateNode(Node):
     def __init__(self, state, parent=None):
         super(GameStateNode, self).__init__(parent)
@@ -80,9 +74,12 @@ class GameStateNode(Node):
         self.state = state
         self.teams = gamestate.teams
 
-        # [(action, uct_score)]
-        self.my_actions = [(a, float("inf")) for a in state.get_legal_actions(teams[0])]
-        self.opp_actions = [(b, float("inf")) for b in state.get_legal_actions(teams[1])]
+        self.my_actions_n = {a: [0.0, 0.0] for a in state.get_legal_actions(teams[0])}
+        self.opp_actions_n = {b: [0.0, 0.0] for b in state.get_legal_actions(teams[1])}
+
+        # {action : uct_score}
+        self.my_actions = {a : float("inf") for a in state.get_legal_actions(teams[0])}
+        self.opp_actions = {b : float("inf") for b in state.get_legal_actions(teams[1])}
 
         # {(my_action, opp_action) : action pair node}
         self.children_actionpairs = {}
@@ -91,6 +88,32 @@ class GameStateNode(Node):
         self.children_gamestates = {}
         if parent is not None:
             self.parent.parent.children_gamestates[state.to_tuple()] = self
+
+    def increment(self, outcome, action_pair):
+        self.times_visited += 1
+        self.wins += outcome
+        self.my_actions_n[action_pair[0]][0] += outcome
+        self.opp_actions_n[action_pair[1]][0] += outcome
+        self.my_actions_n[action_pair[0]][1] += 1
+        self.opp_actions_n[action_pair[1]][1] += 1
+
+    def update_uct_scores(self, action_pair, mean):
+        C = 0.5
+
+        my_action = action_pair[0]
+        opp_action = action_pair[1]
+
+        my_n = self.my_actions_n[my_action][1]
+        opp_n = self.opp_actions_n[opp_action][1]
+
+        my_mean = self.my_actions_n[my_action][0] / my_n
+        opp_mean = self.opp_actions_n[opp_action][0] / opp_n
+
+        my_explore = 2 * C * np.sqrt(2 * np.log(self.times_visited) / my_n)
+        opp_explore = 2 * C * np.sqrt(2 * np.log(self.times_visited) / opp_n)
+
+        self.my_actions[my_action] = my_mean + my_explore
+        self.opp_actions[opp_action] = opp_mean + opp_explore
 
 
 class ActionPairNode(Node):
